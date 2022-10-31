b70feb8fdd938f526c45858d069a3377e8601cb3
frytolnacestach <frytolnacestach@gmail.com>
2022-10-31 20:15:29 +0100

    get url clanky 4

--- express/server.js
+++ express/server.js
@@ -5,29 +5,10 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-
 //clanky
-app.use('/clanky', (req, res) => res.sendFile(path.join(__dirname, '../json/clanky.json')));
-
-/*app.get('/clanky', (req, res) => {
+app.get('/clanky', (req, res) => {
     const clanky = require('../json/clanky.json')
     res.json(clanky)
-});*/
-
-app.get('/clanky', (req, res) => {
-    res.json({msg: 'Hello World :D'})
 })
 
 module.exports = app;
