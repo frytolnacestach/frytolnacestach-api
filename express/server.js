72f80148694f3195f6ebeb820f96bbeee2d8fc58
frytolnacestach <frytolnacestach@gmail.com>
2022-10-31 23:35:14 +0100

    clean code

--- express/server.js
+++ express/server.js
@@ -5,9 +5,6 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-//const clanky = require('../json/clanky.json')
-//const clankyDemo = require('../json/clanky-demo.json')
-
 const router = express.Router();
 router.get('/', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' })
@@ -20,17 +17,6 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')))
-
-//clanky
-app.get('/clanky', (req, res) => {
-    const clanky = require('../json/clanky.json')
-    res.json(clanky)
-})
-
-app.get('/clanky-demo', (req, res) => {
-    const clanky = require('../json/clanky-demo.json')
-    res.json(clanky)
-})
 
 module.exports = app;
 module.exports.handler = serverless(app);
