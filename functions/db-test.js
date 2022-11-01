07c95eef53b211736e95955ea0d6859c8259d91a
frytolnacestach <frytolnacestach@gmail.com>
2022-11-01 23:44:43 +0100

    test (neverending story)

--- functions/db-test.js
+++ functions/db-test.js
@@ -1,47 +1,67 @@
-/*const {createPool} = require('mysql')
+const faunadb = require('faunadb')
+const q = faunadb.query
 
-const pool = createPool({
-    host: "a054um.forpsi.com",
-    user: "f162838",
-    password: "ErUJe3US",
-    connectionLimit: 10
+const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SECRET_KEY,
+    domain: 'db.fauna.com',
+    port: 443,
+    scheme: 'https',
 })
 
-pool.query(`select * from apidb.users`, (err, res) => {
-    return console.log(res)
-})*/
+exports.handler = async (event, context) => {    
+    const clanky = client.query(
+        q.Get(q.Collection('posts'), '347140487201686092')
+    )
+    .then((ret) => console.log(ret))
+    .catch((err) => console.error(
+        'Error: [%s] %s: %s',
+        err.name,
+        err.message,
+        err.errors()[0].description,
+    ))
 
+    return {
+        statusCode: 200,
+        body: JSON.stringify(clanky) + "C2:" + clanky + "C3:" + {clanky}
+    }   
+}
 
-exports.handler = async (event, context) => {
-    var faunadb = require('faunadb')
-    var q = faunadb.query
+    /*
+    const {
+        Ref,
+        Paginate,
+        Get,
+        Match,
+        Index,
+        Create,
+        Collection,
+        Join,
+        Call,
+        Function: Fn,
+    } = faunadb.query;
 
-    const client = new faunadb.Client({
-        secret: '347139750152372818',
-        domain: 'db.fauna.com',
-        port: 443,
-        scheme: 'https',
-    })
+    const clanky = await client.query(
+        Get(
+            Ref(
+                Collection('posts')
+            )
+        )
+    )*/
 
-
-    /*const clanky = await faunaClient.query(
+/*const clanky = await faunaClient.query(
         q.Map(
           q.Paginate(q.Match(q.Index("all_posts"), true)),
           q.Lambda("userRef", q.Get(q.Var("userRef")))
         )
       )*/
 
-      const clanky = client.query(
-        q.Get(q.Ref(q.Collection('posts'), '347140487201686092'))
-      )
-      .then((ret) => console.log(ret))
-      .catch((err) => console.error(
-        'Error: [%s] %s: %s',
-        err.name,
-        err.message,
-        err.errors()[0].description,
-      ))
-
+    /*
+      const clanky = client.query(q.Get(q.Ref(q.Collection('posts'), '347140487201686092')))
+      return {
+        statusCode: 200,
+        body: JSON.stringify(clanky),
+      };*/
+      
 /*
       const clanky = client.query(
         q.Get(q.Index('all_posts'))
@@ -65,14 +85,6 @@ exports.handler = async (event, context) => {
         err.message,
         err.errors()[0].description,
       ))*/
-
-      console.log("testy: " + clanky)
-      console.log(JSON.stringify(clanky))
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(clanky)
-    }
 
     
     
@@ -103,9 +115,8 @@ exports.handler = async (event, context) => {
         statusCode: 200,
         body: JSON.stringify(clanky)
     }*/
-    
-   
-}
+
+
 /*
 const { createPool } = require('mysql')
 
