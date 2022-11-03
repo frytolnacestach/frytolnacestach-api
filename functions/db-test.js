7c70ed7c803c73e24b5c0801005f6fb0e293fd0f
frytolnacestach <frytolnacestach@gmail.com>
2022-11-03 10:23:28 +0100

    Faunadb - dont function to json

--- functions/db-test.js
+++ functions/db-test.js
@@ -2,7 +2,7 @@ const faunadb = require('faunadb')
 const q = faunadb.query
 
 const client = new faunadb.Client({
-    secret: 'fnAE0WVDYOACSxIJL219MKm7uGDyyNt_itjoew5i',
+    secret: process.env.FAUNADB_SECRET_KEY,
     domain: 'db.fauna.com',
     port: 443,
     scheme: 'https',
@@ -23,146 +23,46 @@ exports.handler = async (event, context) => {
         Function: Fn,
     } = faunadb.query;
 
-    //post
-    const clanek = client.query(
-        q.Get(q.Ref(q.Collection('posts'), '347140487201686092'))
-    )
-    .then((ret) => console.log(ret))
-    .catch((err) => console.error(
-        'Error: [%s] %s: %s',
-        err.name,
-        err.message,
-        err.errors()[0].description,
-    ))
 
-    //posts
-    const clanky = client.query(
-        q.Index('all_posts')
+    console.log("______________________ START_4");
+
+    //post slug
+    /*const clanekid = client.query(
+        q.Get(
+          q.Match(q.Index('posts_by_slug'), 'svet-meduz')
+        )
       )
-      .then((ret) => console.log(ret))
-      .catch((err) => console.error(
-        'Error: [%s] %s: %s',
-        err.name,
-        err.message,
-        err.errors()[0].description,
-      ))
+      .then((ret) => console.log(ret))*/
+
+    //clanky
+    const jsonGet = client.query(
+        q.Map(
+            q.Paginate(q.Match(q.Index("all_posts"))),q.Lambda("X", q.Get(q.Var("X")))
+        )
+    )
+    .then((ret) => {
+        return ret.data
+    })
+    
+    const printJson = () => {
+        jsonGet.then((a) => {
+          let json = a
+          console.log(JSON.stringify(json))
+          return JSON.stringify(json)
+        });
+      };
 
     return {
         statusCode: 200,
-        body: "C1:" + JSON.stringify(clanek) + "C2:" + clanek + "C3:" + {clanek} + "C4:" + JSON.stringify(clanky) + "C5:" + clanky + "C6:" + {clanky}
+        body: JSON.stringify(printJson())
     }
 }
 
     /*
+    const printAddress = async () => {
 
-    client.query(
-        q.CreateCollection({name: "atest"})
-    )
+            const a = await address;
+        console.log(JSON.stringify(a))
+        return JSON.stringify(a)
 
-    const {
-        Ref,
-        Paginate,
-        Get,
-        Match,
-        Index,
-        Create,
-        Collection,
-        Join,
-        Call,
-        Function: Fn,
-    } = faunadb.query;
-
-    const clanky = await client.query(
-        Get(
-            Ref(
-                Collection('posts')
-            )
-        )
-    )*/
-
-/*const clanky = await faunaClient.query(
-        q.Map(
-          q.Paginate(q.Match(q.Index("all_posts"), true)),
-          q.Lambda("userRef", q.Get(q.Var("userRef")))
-        )
-      )*/
-
-    /*
-      const clanky = client.query(q.Get(q.Ref(q.Collection('posts'), '347140487201686092')))
-      return {
-        statusCode: 200,
-        body: JSON.stringify(clanky),
-      };*/
-      
-/*
-      const clanky = client.query(
-        q.Get(q.Index('all_posts'))
-      )
-      .then((ret) => console.log(ret))
-      .catch((err) => console.error(
-        'Error: [%s] %s: %s',
-        err.name,
-        err.message,
-        err.errors()[0].description,
-      ))
-*/
-      /*
-    const clanky = client.query(
-        q.Get(q.Index('all_posts'))
-      )
-      .then((ret) => ret)
-      .catch((err) => console.error(
-        'Error: [%s] %s: %s',
-        err.name,
-        err.message,
-        err.errors()[0].description,
-      ))*/
-
-    
-    
-    /*const clanky = require('../json/clanky.json')
-
-    const { createPool } = require('mysql')
-
-    const pool = createPool({
-        host: "a054um.forpsi.com",
-        user: "f162838",
-        password: "ErUJe3US",
-        database: "f162838",
-        connectionLimit: 10
-    })
-
-    pool.query(`select * from posts where id = 1`, function(err, result, fields) {
-        if (err) {
-            console.log("test select - error");
-            return console.log(err);
-        }
-        console.log("test select");
-        return console.log(result);
-    })
-
-    module.exports = pool;
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(clanky)
-    }*/
-
-
-/*
-const { createPool } = require('mysql')
-
-const pool = createPool({
-    host: "a054um.forpsi.com",
-    user: "f162838",
-    password: "ErUJe3US",
-    database: "f162838",
-    connectionLimit: 10
-})
-
-pool.query(`select * from posts`, function(err, result, fields) {
-    if (err) {
-        return console.log(err);
-    }
-    return console.log(result);
-})*/
+    };*/
