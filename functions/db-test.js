212dd492f8cfb5f98a8f6ec75820c69cd8a26f1b
frytolnacestach <frytolnacestach@gmail.com>
2022-11-03 16:09:47 +0100

    JSON.stringify - data + error

--- functions/db-test.js
+++ functions/db-test.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
 
     return {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify(data) + error
     }
     
 }
