f33b9c1eaf17475da50e57afc26d77b572625c4f
frytolnacestach <frytolnacestach@gmail.com>
2022-11-04 00:27:03 +0100

    test - get slug 4 - getPostSlug.res

--- functions/post.js
+++ functions/post.js
@@ -23,6 +23,6 @@ exports.handler = async (event, context) => {
 
     return {
         statusCode: 200,
-        body: JSON.stringify(data) + getPostSlug
+        body: JSON.stringify(data) + getPostSlug.res
     } 
 }
