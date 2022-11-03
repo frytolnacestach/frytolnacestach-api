a383b954f60b6fdfe51685a2b8ba285db6d65722
frytolnacestach <frytolnacestach@gmail.com>
2022-11-04 00:09:14 +0100

    test - get slug 2 - error and getPostSlug

--- functions/post.js
+++ functions/post.js
@@ -23,6 +23,6 @@ exports.handler = async (event, context) => {
 
     return {
         statusCode: 200,
-        body: JSON.stringify(data) + JSON.stringify(error)
+        body: JSON.stringify(data) + JSON.stringify(error) + getPostSlug
     } 
 }
