1e5e5111785c0707e50d67c2c0312a4e39898dd8
frytolnacestach <frytolnacestach@gmail.com>
2022-11-04 23:50:46 +0100

    Back to last version

---
+++ functions/post.js
@@ -0,0 +1,35 @@
+import { createClient } from '@supabase/supabase-js'
+
+import express from "express";
+
+const app =  express();
+
+const supabaseUrl = 'https://qdjxqerpuvcwnbiqojnv.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+exports.handler = async (event, context) => {
+    
+    app.get(':postSlug', async (req, res) =>  {
+        var postSlug = req.params.postSlug;
+
+        const { data, error } = await supabase
+        .from('posts')
+        .select()
+        .eq('slug', postSlug)
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        }
+    
+        return response
+        
+    })
+
+
+    /*return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    }*/
+}
--- functions/posts.js
+++ functions/posts.js
@@ -1,19 +1,11 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qdjxqerpuvcwnbiqojnv.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-const express = require('express');
-const cors = require('cors');
+exports.handler = async (event, context) => {
 
-const router = express.Router();
-const body_parser = require('body-parser')
-
-router.use(cors());
-router.use(body_parser.json())
-
-async function start() {
     const { data, error } = await supabase
     .from('posts')
     .select()
@@ -21,8 +13,5 @@ async function start() {
     return {
         statusCode: 200,
         body: JSON.stringify(data)
-    }
+    } 
 }
-start()
-
-module.exports = router;
