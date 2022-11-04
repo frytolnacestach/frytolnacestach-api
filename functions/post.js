cbc79759503c94e48beb613f366a1331880da5aa
frytolnacestach <frytolnacestach@gmail.com>
2022-11-04 21:37:32 +0100

    test - supabase in app.get 10

--- functions/post.js
+++ functions/post.js
@@ -23,7 +23,7 @@ exports.handler = async (event, context) => {
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify({"name": "Michal"})
     }
 
     return response
