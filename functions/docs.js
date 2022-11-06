5a3809f0053e2cd92689d9d40f3c2a2fd65152b3
frytolnacestach <frytolnacestach@gmail.com>
2022-11-06 10:37:55 +0100

    test - Other load functions

---
+++ functions/docs.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app =  express();
+
+module.exports = async function(req, res) {
+    
+    const pageBody = `
+        <header class="o-header">
+            <nav class="m-nav">
+                <ul class="m-nav__items">
+                    <li class="m-nav__item">
+                        <a class="m-nav__link" href="/">Úvod</a>
+                    </li>
+                    <li class="m-nav__item">
+                        <a class="m-nav__link" href="/.netlify/pages/docs">Dokumentace</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <div class="o-hero">
+            <h1 class="o-hero__title">
+                <span class="o-hero__title-api">Documentace API</span>
+                <br>Frytol na cestách
+            </h1>
+        </div>`
+    
+    return {
+        statusCode: 200,
+        body: pageBody
+    }
+}
