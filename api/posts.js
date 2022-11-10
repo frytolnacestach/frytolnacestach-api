1fd0c21f783fafd0a90c6eea057f534d91f7fc1c
frytolnacestach <frytolnacestach@gmail.com>
2022-11-10 19:28:02 +0100

    Test - Vercel 5 - posts

--- api/posts.js
+++ api/posts.js
@@ -1,15 +1,29 @@
+import { createClient } from '@supabase/supabase-js'
+
 const express = require("express");
 const router = express.Router();
 
+const supabaseUrl = 'https://qdjxqerpuvcwnbiqojnv.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 router.get("/", async (req, res) => {
   try {
+
+    const { data, error } = await supabase
+    .from('posts')
+    .select()
+
     res.json({
       status: 200,
-      message: "Get data has successfully",
+      message: data,
     });
+
   } catch (error) {
+
     console.error(error);
     return res.status(500).send("Server error");
+    
   }
 });
 
