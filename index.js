84ee8503c132fd3deb57c15c38b9a9be89c5fbe5
frytolnacestach <frytolnacestach@gmail.com>
2022-11-10 19:54:29 +0100

    Add index and docs html

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express')
 
 const app = express()
 
+const index = require("./pages/index.html");
+const docs = require("./pages/docs.html");
 const post = require("./api/post");
 const posts = require("./api/posts");
 
 app.use(express.json({ extended: false }));
 
+app.use("/pages/index", post);
+app.use("/pages/docs", post);
 app.use("/api/post", post);
 app.use("/api/posts", posts);
 
