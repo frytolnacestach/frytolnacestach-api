c6e9d694782f9c5a767515717e2270beb10e7dcf
frytolnacestach <frytolnacestach@gmail.com>
2022-11-10 19:23:33 +0100

    Test - Vercel 3 - posts

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express')
 const app = express()
 
 const product = require("./api/product");
+const posts = require("./api/posts");
 
 app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
+app.use("/api/posts", posts);
 
 const PORT = process.env.PORT || 5050
 
