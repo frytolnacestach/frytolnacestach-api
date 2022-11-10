41fd63bf37e96a682ef836002c6d84e26f266871
frytolnacestach <frytolnacestach@gmail.com>
2022-11-10 19:30:24 +0100

    Post and post js for Vercel

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 
 const app = express()
 
-const product = require("./api/product");
+const post = require("./api/post");
 const posts = require("./api/posts");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+app.use("/api/post", post);
 app.use("/api/posts", posts);
 
 const PORT = process.env.PORT || 5050
