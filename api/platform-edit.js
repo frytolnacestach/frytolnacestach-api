edc43967846924764199586feb70e9203b0ede65
frytolnacestach <frytolnacestach@gmail.com>
2022-11-25 23:28:31 +0100

    edit spaces

--- api/platform-edit.js
+++ api/platform-edit.js
@@ -8,28 +8,26 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 router.post("/", async (req, res) => {
-  try {
+	try {
 
-    const { data, error } = await supabase
-    .from('platforms')
-    .update({
-      slug: req.body.body.slug,
-      name: req.body.body.name,
-      perex: req.body.body.perex,
-      url: req.body.body.url,
-      facts: JSON.parse(req.body.body.facts),
-      date: req.body.body.date
-    })
-    .eq('slug', req.body.body.slug)
+		const { data, error } = await supabase
+		.from('platforms')
+		.update({
+			slug: req.body.body.slug,
+			name: req.body.body.name,
+			perex: req.body.body.perex,
+			url: req.body.body.url,
+			facts: JSON.parse(req.body.body.facts),
+			date: req.body.body.date
+		})
+		.eq('slug', req.body.body.slug)
 
-    res.send(JSON.stringify(data))
+		res.send(JSON.stringify(data))
 
-  } catch (error) {
-
-    console.error(error);
-    return res.status(500).send("Server error");
-    
-  }
+	} catch (error) {
+		console.error(error);
+		return res.status(500).send("Server error");
+	}
 });
 
 module.exports = router;
