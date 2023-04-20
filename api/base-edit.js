cc262b63526f98fc8f07bcb361f6717a4eaa835a
frytolnacestach <frytolnacestach@gmail.com>
2023-04-20 19:56:08 +0200

    Test with cors 8

--- api/base-edit.js
+++ api/base-edit.js
@@ -17,7 +17,7 @@ router.post("/", async (req, res) => {
 		const { data, error } = await supabase
 		.from('base')
 		.update({
-			iam: "Ahoj, jmenuji se Michal. V osobním životě podnikám jako webový kódér ale už od mala mě to táhlo k natáčení a bavilo mě cestovat a tak jsem tyto dvě věci spojil a v roce 2018 založil Frytol na cestách. Nejdříve jsem se zaměřoval pouze na cestování ale postupně jsem začal točit i názorová videa nebo také stavění Lega.",
+			iam: "TEST - Ahoj, jmenuji se Michal. V osobním životě podnikám jako webový kódér ale už od mala mě to táhlo k natáčení a bavilo mě cestovat a tak jsem tyto dvě věci spojil a v roce 2018 založil Frytol na cestách. Nejdříve jsem se zaměřoval pouze na cestování ale postupně jsem začal točit i názorová videa nebo také stavění Lega.",
 			donate: "Podpořit mě můžetě samotním sledování, odběrem a sdílením mé tvorby. Každa reakce v podobě lajku nebo komentáře je dúležitá a uspokoji algoritmus YouTube nebo jiné platformy. Pro ty ostatní co by mě chtěli podpořit finančně jsem založil Patreon."
 		})
 		.eq('id', 1)
