9e99ad506f82a25a72ae5324d0c59b1b594ecb1c
frytolnacestach <frytolnacestach@gmail.com>
2023-04-20 20:10:42 +0200

    cors plugin delete

--- api/base-edit.js
+++ api/base-edit.js
@@ -1,11 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const express = require("express");
-const cors = require('cors');
 const router = express.Router();
-
-const app = express();
-app.use(cors());
 
 const supabaseUrl = 'https://qdjxqerpuvcwnbiqojnv.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
@@ -17,8 +13,8 @@ router.post("/", async (req, res) => {
 		const { data, error } = await supabase
 		.from('base')
 		.update({
-			iam: "TEST - Ahoj, jmenuji se Michal. V osobním životě podnikám jako webový kódér ale už od mala mě to táhlo k natáčení a bavilo mě cestovat a tak jsem tyto dvě věci spojil a v roce 2018 založil Frytol na cestách. Nejdříve jsem se zaměřoval pouze na cestování ale postupně jsem začal točit i názorová videa nebo také stavění Lega.",
-			donate: "Podpořit mě můžetě samotním sledování, odběrem a sdílením mé tvorby. Každa reakce v podobě lajku nebo komentáře je dúležitá a uspokoji algoritmus YouTube nebo jiné platformy. Pro ty ostatní co by mě chtěli podpořit finančně jsem založil Patreon."
+			iam: req.body.iam,
+			donate: req.body.donate
 		})
 		.eq('id', 1)
 
@@ -32,6 +28,4 @@ router.post("/", async (req, res) => {
 	}
 });
 
-app.use(router);
-
-module.exports = app;
+module.exports = router;
