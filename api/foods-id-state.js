9f3681c77733bb9f810d0c9a906d49aef4962100
frytolnacestach <frytolnacestach@gmail.com>
2023-05-25 22:41:50 +0200

    API - foods-id-state 9

--- api/foods-id-state.js
+++ api/foods-id-state.js
@@ -8,15 +8,15 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 router.get('/:slug', async (req, res) => {
-  var id = req.params.slug
+  const id = req.params.slug
 
   try {
-
-    const { data, error } = await supabase
-    .from('foods')
-    .select()
-    .eq('ids_states->id', id)
-    .order('name', { ascending: true })
+    let data;
+    const { data: responseData, error } = await supabase
+      .from('foods')
+      .select()
+      .eq('ids_states->id', id)
+      .order('name', { ascending: true });
 
     if (Array.isArray(responseData)) {
       data = responseData;
@@ -26,13 +26,10 @@ router.get('/:slug', async (req, res) => {
       data = [];
     }
 
-    res.send(JSON.stringify(data))
-
+    res.send(JSON.stringify(data));
   } catch (error) {
-
     console.error(error);
     return res.status(500).send("Server error");
-    
   }
 });
 
