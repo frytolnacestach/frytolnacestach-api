4f68370ab7c9ce088b20de97a886af0b19859c2b
frytolnacestach <frytolnacestach@gmail.com>
2023-05-26 09:37:56 +0200

    API - foods-id-state 47

--- api/foods-id-state.js
+++ api/foods-id-state.js
@@ -15,7 +15,7 @@ router.get('/:slug', async (req, res) => {
     const { data, error } = await supabase
     .from('foods')
     .select()
-    .contains("ids_states3", JSON.stringify([{ id: 56 }]))
+    .contains("ids_states3", JSON.stringify([{ id: 57 }]))
 
     res.send(JSON.stringify(data))
 
