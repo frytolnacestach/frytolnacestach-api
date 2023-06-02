ebe859022736ab31503b7c7539e9389a82b0d5f0
frytolnacestach <frytolnacestach@gmail.com>
2023-06-02 21:43:18 +0200

    Test - Registration with send email 10

--- api/user-registration.js
+++ api/user-registration.js
@@ -1,67 +1,73 @@
-import { createClient } from '@supabase/supabase-js'
-
-//const axios = require('axios');
-
+const express = require("express");
+const cors = require("cors");
+const { createClient } = require('@supabase/supabase-js');
+const axios = require('axios');
 const bcrypt = require('bcrypt');
 
-const express = require("express");
+const app = express();
 const router = express.Router();
 
 const supabaseUrl = 'https://qdjxqerpuvcwnbiqojnv.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-router.post("/", async (req, res) => {
+app.use(cors()); // Přidání middleware pro CORS
+app.use(express.json()); // Povolit parsování JSON dat
 
-    // Kontrola existence uživatele
-    const { data: existingUser, error: existingError } = await supabase
+router.post("/", async (req, res) => {
+  // Kontrola existence uživatele
+  const { data: existingUser, error: existingError } = await supabase
     .from('users_test')
     .select('id')
     .eq('email', req.body.email)
     .limit(1);
 
-    if (existingError) {
-        console.error(existingError);
-        return res.status(500).send("Server error");
+  if (existingError) {
+    console.error(existingError);
+    return res.status(500).send("Server error");
+  }
+
+  if (existingUser.length > 0) {
+    return res
+      .status(400)
+      .send("Uživatel s touto e-mailovou adresou již existuje.");
+  }
+
+  // Vytvoření účtu
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const { error } = await supabase
+      .from('users_test')
+      .insert({
+        email: req.body.email,
+        password: hashedPassword,
+        nickname: req.body.nickname
+      });
+
+    if (error) {
+      console.error(error);
+      return res.status(500).send("Server error");
     }
 
-    if (existingUser.length > 0) {
-        return res.status(400).send("Uživatel s touto e-mailovou adresou již existuje.");
-    }
-
-
-    //Vytvoření účtu
+    // Odeslat registrační e-mail
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-        const { error } = await supabase
-        .from('users_test')
-        .insert({ 
-			email: req.body.email,
-            password: hashedPassword,
-			nickname: req.body.nickname
-        })
-
-        if (error) {
-            console.error(error);
-            return res.status(500).send("Server error");
-        }
-/*
-        // Odeslat registrační e-mail
-        try {
-            await axios.post('https://frytolnacestach-mail.vercel.app/api/registration', { email: req.body.email });
-            return res.status(201).send("Účet vytvořen, registrační e-mail odeslán.");
-        } catch (emailError) {
-            console.error('Chyba při odesílání registračního e-mailu:', emailError);
-            return res.status(500).send("Server error");
-        }*/
-
-        return res.status(201).send("Učet vytvořen");
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Server error");
+      await axios.post('https://frytolnacestach-mail.vercel.app/api/registration', { email: req.body.email });
+      return res.status(201).send("Účet vytvořen, registrační e-mail odeslán.");
+    } catch (emailError) {
+      console.error('Chyba při odesílání registračního e-mailu:', emailError);
+      return res.status(500).send("Server error");
     }
-    
+
+    return res.status(201).send("Účet vytvořen");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
 });
 
-module.exports = router;
+app.use("/", router);
+
+app.listen(3000, () => {
+  console.log("Server běží na portu 3000");
+});
