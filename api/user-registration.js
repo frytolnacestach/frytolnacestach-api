24239c417570b34772ea036c80378156f5f2125b
frytolnacestach <frytolnacestach@gmail.com>
2023-06-02 21:28:57 +0200

    Test - Registration with send email 2

--- api/user-registration.js
+++ api/user-registration.js
@@ -48,13 +48,15 @@ router.post("/", async (req, res) => {
         }
 
         // Odeslat registrační e-mail
-        try {
+        /*try {
             await axios.post('https://frytolnacestach-mail.vercel.app/api/registration', { email: req.body.email });
             return res.status(201).send("Účet vytvořen, registrační e-mail odeslán.");
         } catch (emailError) {
             console.error('Chyba při odesílání registračního e-mailu:', emailError);
             return res.status(500).send("Server error");
-        }
+        }*/
+
+        return res.status(201).send("Učet vytvořen");
     } catch (error) {
         console.error(error);
         return res.status(500).send("Server error");
