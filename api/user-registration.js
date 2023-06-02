d7560a675daed1327a2b075d0f9d51214e638ec4
frytolnacestach <frytolnacestach@gmail.com>
2023-06-02 21:36:35 +0200

    Test - Registration with send email 7

--- api/user-registration.js
+++ api/user-registration.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-//const axios = require('axios');
+const axios = require('axios');
 
 const bcrypt = require('bcrypt');
 
@@ -42,11 +42,11 @@ router.post("/", async (req, res) => {
 			nickname: req.body.nickname
         })
 
-        /*if (error) {
+        if (error) {
             console.error(error);
             return res.status(500).send("Server error");
         }
-
+/*
         // Odeslat registrační e-mail
         try {
             await axios.post('https://frytolnacestach-mail.vercel.app/api/registration', { email: req.body.email });
