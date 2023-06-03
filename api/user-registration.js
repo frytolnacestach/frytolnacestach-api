7c7424ea1750a516148b237f47d4c96a7845237a
frytolnacestach <frytolnacestach@gmail.com>
2023-06-03 02:01:04 +0200

    Test - Registration with send email 51

--- api/user-registration.js
+++ api/user-registration.js
@@ -106,7 +106,7 @@ router.post("/", async (req, res) => {
             return res.status(500).send('Chyba připojení k API MAIL');
         }*/
 
-        //return res.status(201).send("Učet vytvořen");
+        return res.status(201).send("Učet vytvořen");
     } catch (error) {
         console.error(error);
         return res.status(500).send("Server error");
