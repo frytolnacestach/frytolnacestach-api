e6d34ba40ceeae1857f406a5c24981e86575a3a6
frytolnacestach <frytolnacestach@gmail.com>
2023-06-04 09:24:41 +0200

    Change status after activation account 10

--- api/user-activation.js
+++ api/user-activation.js
@@ -14,23 +14,24 @@ router.post("/:email/:code_activation", async (req, res) => {
     try {
         const { data, error } = await supabase
         .from('users_test')
-        .eq('email', email)
-        .eq('code_activation', codeActivation)
-        .eq('status', 2)
+        .select()
+        .eq('email', email);
 
-        if (error) {
-            console.error(error);
-            return res.status(500).send("Server error");
-        }
+    if (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
 
-        if (data.length === 0) {
-            return res.status(404).send("User not found");
-        }
+    if (data.length === 0) {
+        return res.status(404).send("User not found");
+    }
 
-        res.json({
-            status: 200,
-            message: data,
-        });
+    const user = data[0];
+    if (password !== user.password) {
+        return res.status(401).send("Invalid password");
+    }
+
+    res.sendStatus(200);
     } catch (error) {
         console.error(error);
         return res.status(500).send("Server error");
