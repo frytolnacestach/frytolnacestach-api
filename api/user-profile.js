7bcaf723461bd1a41afa86d6ba2c46317d271966
frytolnacestach <frytolnacestach@gmail.com>
2023-06-04 14:29:30 +0200

    API - Profile (data and edit) 5

--- api/user-profile.js
+++ api/user-profile.js
@@ -7,19 +7,22 @@ const supabaseUrl = 'https://qdjxqerpuvcwnbiqojnv.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-router.get("/:email", async (req, res) => {
+router.get('/:email', async (req, res) => {
     var email = req.params.email
-
     try {
+
         const { data, error } = await supabase
         .from('users_test')
         .select()
-        .eq('email', email);
+        .eq('email', email)
 
         res.send(JSON.stringify(data))
+
     } catch (error) {
+
         console.error(error);
         return res.status(500).send("Server error");
+
     }
 });
 
