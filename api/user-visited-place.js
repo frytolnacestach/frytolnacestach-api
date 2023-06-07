68d54aa964c69bba9509cae2284dec1f09ffff1c
frytolnacestach <frytolnacestach@gmail.com>
2023-06-07 17:43:02 +0200

    select id from users in user-visited-place

--- api/user-visited-place.js
+++ api/user-visited-place.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
     try {
         const { data, error } = await supabase
         .from('users')
-        .select()
+        .select('id')
         .eq('email', email)
         .eq('password', passwordHash)
 
