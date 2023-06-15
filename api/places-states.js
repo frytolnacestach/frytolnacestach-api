d815bbe3966c96263f78938753a65560f4f302a2
frytolnacestach <frytolnacestach@gmail.com>
2023-06-15 22:19:37 +0200

    places-states edit for page range 20

--- api/places-states.js
+++ api/places-states.js
@@ -18,7 +18,7 @@ router.get("/", async (req, res) => {
     
     if(page && items) {
       const itemsStart = (page * items) - items;
-      const itemsEnd = (page * items) + items - 1;
+      const itemsEnd = itemsStart + items - 1;
 
       const response = await supabase
       .from('places_states')
