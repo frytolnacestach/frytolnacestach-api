bdf769f8c4ee1955d95412678245c447f159e4d5
frytolnacestach <frytolnacestach@gmail.com>
2023-06-20 21:28:14 +0200

    Filter showType for (brands, faunas, floras and foods)

--- api/faunas-id-state.js
+++ api/faunas-id-state.js
@@ -9,22 +9,26 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 router.get('/:slug', async (req, res) => {
   var id = req.params.slug
+  const showType = req.query.showType
+
+  //setting Select
+  let supabaseSelect;
+  if (showType === "list"){
+    supabaseSelect = 'id, id_image_cover, slug, name';
+  }
 
   try {
-
     const { data, error } = await supabase
     .from('fauna')
-    .select()
+    .select(supabaseSelect)
     .contains("ids_states", JSON.stringify([{ id: parseInt(id) }]))
 
     res.send(JSON.stringify(data))
-
   } catch (error) {
-
     console.error(error);
     return res.status(500).send("Server error");
-    
   }
+
 });
 
 module.exports = router;
