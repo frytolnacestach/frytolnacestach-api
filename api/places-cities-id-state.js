075ab45043f1c9f8f1220ace080d49f055e0fcf0
frytolnacestach <frytolnacestach@gmail.com>
2023-06-20 17:39:24 +0200

    showType for cities in state

--- api/places-cities-id-state.js
+++ api/places-cities-id-state.js
@@ -8,23 +8,28 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 router.get('/:slug', async (req, res) => {
+  const showType = req.query.showType
   var id = req.params.slug
-  try {
 
+  //setting Select
+  let supabaseSelect;
+  if (showType === "list"){
+    supabaseSelect = 'id, id_image_cover, slug, type_place, name';
+  }
+
+  try {
     const { data, error } = await supabase
     .from('places_cities')
-    .select()
+    .select(supabaseSelect)
     .eq('id_state', id)
     .order('name', { ascending: true })
 
     res.send(JSON.stringify(data))
-
   } catch (error) {
-
     console.error(error);
     return res.status(500).send("Server error");
-    
   }
+  
 });
 
 module.exports = router;
