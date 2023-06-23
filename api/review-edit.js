5f3fb30da0f067775f37b42e9b72cb16090113a9
frytolnacestach <frytolnacestach@gmail.com>
2023-06-23 02:59:52 +0200

    Edit for review-edit

--- api/review-edit.js
+++ api/review-edit.js
@@ -9,15 +9,52 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 router.post("/", async (req, res) => {
 	
+	// user
+	var email = req.body.email
+    var passwordHash = req.body.password_hash
+	// other var
+	//let dateEdit
+	let status = 1
+	let idUser
+	
 	try {
+		// Check user
 		const { data, error } = await supabase
-		.from('users_place_review_duplicate')
-		.update({
-			// ZDE DOPLNIT NUTNE PRVKY
-		})
-		.eq('slug', req.body.slug)
+        .from('users')
+        .select('id')
+        .eq('email', email)
+        .eq('password', passwordHash)
 
-		res.send(JSON.stringify(data))
+        if (error) {
+            console.error(error);
+            return res.status(500).send("Server error");
+        }
+
+        if (data.length === 0) {
+            return res.status(404).send('Uživatel neexistuje');
+        } else {
+			idUser = data[0].id;
+
+			try {
+				// add review
+				const { data, error } = await supabase
+				.from('users_place_review_duplicate')
+				.insert({
+					//date_edit: dateEdit,
+					rating: req.body.rating,
+					text: req.body.text,
+				})
+				.eq('id_place', req.body.id_place)
+				.eq('id_user', req.body.idUser)
+				.eq('type', req.body.type)
+
+				return res.status(201).send("Záznam uložen");
+			} catch (error) {
+				console.error(error);
+				return res.status(500).send("Server error");
+			}
+
+		}
 	} catch (error) {
 		console.error(error);
 		return res.status(500).send("Server error");
