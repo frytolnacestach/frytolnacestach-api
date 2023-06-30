965afc9bc45b0eeaf672f237d93c40af2297a4db
frytolnacestach <frytolnacestach@gmail.com>
2023-07-01 01:23:49 +0200

    Add filter to top-places

--- api/top-places.js
+++ api/top-places.js
@@ -8,21 +8,29 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 router.get("/", async (req, res) => {
+  const typePlace = req.query.typePlace || ''
+
   try {
+      // Base query
+      let query = supabase.from('top_places')
+        .select()
+        .order('id', { ascending: false })
+  
+      // ADD typePlace
+      if (typePlace !== '' && typePlace !== null) {
+        query = query.eq('type', typePlace);
+      }
 
-    const { data, error } = await supabase
-    .from('top_places')
-    .select()
-    .order('id', { ascending: false })
-
-    res.send(JSON.stringify(data))
-
+  
+      // DATA
+      const { data, error } = await query;
+  
+      res.send(JSON.stringify(data))
   } catch (error) {
-
     console.error(error);
     return res.status(500).send("Server error");
-    
   }
+  
 });
 
 module.exports = router;
