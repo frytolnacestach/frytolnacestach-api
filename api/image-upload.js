d2e10e8eb909f3bd95fb8c77da768d75e3963bc2
frytolnacestach <frytolnacestach@gmail.com>
2023-07-08 03:23:37 +0200

    TEST - API for upload image 5

--- api/image-upload.js
+++ api/image-upload.js
@@ -1,7 +1,7 @@
-const express = require('express');
+/*const express = require('express');
 const router = express.Router();
 
-/*const FTPClient = require('basic-ftp');
+const FTPClient = require('basic-ftp');
 
 router.post('/', async (req, res) => {
   try {
@@ -24,4 +24,37 @@ router.post('/', async (req, res) => {
     console.error(error);
     return res.status(500).send('Chyba při nahrávání obrázku na jiný server.');
   }
-});*/
+});
+
+import { createClient } from '@supabase/supabase-js'*/
+
+const express = require("express");
+const router = express.Router();
+
+const supabaseUrl = 'https://qdjxqerpuvcwnbiqojnv.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+router.get("/", async (req, res) => {
+  try {
+
+    const { id } = req.query;
+    const ids = id.split(",").map((id) => parseInt(id));
+
+    const { data, error } = await supabase
+    .from('images')
+    .select()
+    .in("id", ids)
+    .order('id', { ascending: false })
+
+    res.send(JSON.stringify(data))
+
+  } catch (error) {
+
+    console.error(error);
+    return res.status(500).send("Server error");
+    
+  }
+});
+
+module.exports = router;
