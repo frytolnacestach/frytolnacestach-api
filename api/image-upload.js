2b0efa7a32dc684306d42b0670645adc94040e04
frytolnacestach <frytolnacestach@gmail.com>
2023-07-08 12:37:22 +0200

    TEST - API for upload image 30

--- api/image-upload.js
+++ api/image-upload.js
@@ -7,11 +7,9 @@ const FTPHost = process.env.FTP_IMAGE_HOST
 const FTPUser = process.env.FTP_IMAGE_USER
 const FTPPass = process.env.FTP_IMAGE_PASS
 
-/*
+
 router.get('/', async (req, res) => {
     try {
-      const client = new FTPSClient();
-  
       const client = new FTPSClient({
         host: FTPHost,
         username: FTPUser,
@@ -28,9 +26,10 @@ router.get('/', async (req, res) => {
       return res.status(500).send('Chyba při připojování k FTP serveru. ftpH:' + FTPHost + 'ftpU:' + FTPUser + 'ftpP:' + FTPPass);
     }
   });
-  */
 
-  router.post('/', async (req, res) => {
+
+  /*
+router.post('/', async (req, res) => {
     try {
       const image = req.files.image;
   
@@ -53,6 +52,6 @@ router.get('/', async (req, res) => {
       return res.status(500).send('Chyba při nahrávání obrázku na jiný server.');
     }
   });
-  
+  */
 
 module.exports = router;
