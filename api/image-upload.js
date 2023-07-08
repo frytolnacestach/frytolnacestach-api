b747c3ad4ad1c49945f1981616d92068289eddb1
frytolnacestach <frytolnacestach@gmail.com>
2023-07-08 04:01:57 +0200

    TEST - API for upload image 11

--- api/image-upload.js
+++ api/image-upload.js
@@ -35,7 +35,7 @@ router.post('/', async (req, res) => {
     return res.status(201).send('Obrázek byl úspěšně nahrán na jiný server.');
   } catch (error) {
     console.error(error);
-    return res.status(500).send('Chyba při nahrávání obrázku na jiný server. velikost:' + imageSize);
+    return res.status(500).send('Chyba při nahrávání obrázku na jiný server. file:');
   }
 });
 
