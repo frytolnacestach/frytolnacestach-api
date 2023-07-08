c7f7be7bbd9f312ae8491f38acedf66fe21a0c94
frytolnacestach <frytolnacestach@gmail.com>
2023-07-08 23:13:22 +0200

    TEST - API for upload image 80

--- api/image-upload.js
+++ api/image-upload.js
@@ -24,14 +24,20 @@ router.post('/', async (req, res) => {
         port: 21,
       });
 
-      await client.cd('/subdoms/image/storage/aaatest');
+      //await client.cd('/subdoms/image/storage/aaatest');
+      //await client.put(image.data, image.name);
 
-      //await client.delete('/subdoms/image/storage/aaatest/test_raw2.png');
+      await client.delete('/subdoms/image/storage/aaatest/test_raw2.png');
 
       //await client.put(image.data, image.name);
 
       //const response = await client.raw('getreply');
 
+
+
+      //const transferSuccessful = response && response.includes('226');
+
+      const response = await client.raw('getreply');
 
 
       //const transferSuccessful = response && response.includes('226');
