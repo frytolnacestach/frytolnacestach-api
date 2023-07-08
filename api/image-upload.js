637288da3603221b0132acb19ae74029153de2c2
frytolnacestach <frytolnacestach@gmail.com>
2023-07-08 05:29:12 +0200

    TEST - API for upload image 28

--- api/image-upload.js
+++ api/image-upload.js
@@ -6,9 +6,11 @@ const FTPSClient = require('ftps');
 const FTPHost = process.env.FTP_IMAGE_HOST
 const FTPUser = process.env.FTP_IMAGE_USER
 const FTPPass = process.env.FTP_IMAGE_PASS
+
 /*
 router.get('/', async (req, res) => {
     try {
+      const client = new FTPSClient();
   
       const client = new FTPSClient({
         host: FTPHost,
@@ -18,33 +20,30 @@ router.get('/', async (req, res) => {
         port: 21,
       });
   
-      await client.ls();
+      client.close();
   
       return res.status(200).send('Připojení k FTP serveru bylo úspěšné. ftpH:' + FTPHost + 'ftpU:' + FTPUser + 'ftpP:' + FTPPass);
     } catch (error) {
       console.error(error);
       return res.status(500).send('Chyba při připojování k FTP serveru. ftpH:' + FTPHost + 'ftpU:' + FTPUser + 'ftpP:' + FTPPass);
     }
-  });*/
-
+  });
+  */
 
 router.post('/', async (req, res) => {
   try {
     const image = req.files.image;
 
-    const client = new FTPSClient();
-    
-    await client.access({
+    const client = new FTPSClient({
       host: FTPHost,
-        username: FTPUser,
-        password: FTPPass,
-        protocol: 'ftp',
-        port: 21,
+      username: FTPUser,
+      password: FTPPass,
+      protocol: 'ftp',
+      port: 21,
     });
 
-    await client.uploadFrom(image.path, '/storage/__test/' + image.name);
-
-    client.close();
+    await client.cd('/storage/__test');
+    await client.put(image.path, image.name);
 
     return res.status(201).send('Obrázek byl úspěšně nahrán na jiný server.');
   } catch (error) {
