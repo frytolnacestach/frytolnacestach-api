2b883c964576c403facef11fd809393d620b8eaa
frytolnacestach <frytolnacestach@gmail.com>
2023-07-08 04:45:03 +0200

    TEST - API for upload image 17

--- api/image-upload.js
+++ api/image-upload.js
@@ -3,21 +3,34 @@ const router = express.Router();
 
 const FTPClient = require('basic-ftp');
 
-router.post('/', async (req, res) => {
-    let imageSize;
+
+router.get('/', async (req, res) => {
+    try {
+      const client = new FTPClient();
   
-try {
+      await client.access({
+        host: process.env.FTP_IMAGE_HOST,
+        user: process.env.FTP_IMAGE_USER,
+        password: process.env.FTP_IMAGE_PASS,
+        port: 21,
+      });
+  
+      client.close();
+  
+      return res.status(200).send('Připojení k FTP serveru bylo úspěšné.');
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send('Chyba při připojování k FTP serveru.');
+    }
+  });
+
+/*
+router.post('/', async (req, res) => {
+  try {
     const image = req.files.image;
 
-    const fs = require('fs');
-    const util = require('util');
-    const stat = util.promisify(fs.stat);
-
-    const imageStats = await stat(image.path);
-    imageSize = imageStats.size;
-
     const client = new FTPClient();
-
+    
     await client.access({
       host: process.env.FTP_IMAGE_HOST,
       user: process.env.FTP_IMAGE_USER,
@@ -32,12 +45,8 @@ try {
     return res.status(201).send('Obrázek byl úspěšně nahrán na jiný server.');
   } catch (error) {
     console.error(error);
-    if (imageSize) {
-        return res.status(500).send(`Chyba při nahrávání obrázku na jiný server. Velikost souboru: ${imageSize} bajtů.`);
-      } else {
-        return res.status(500).send('Chyba při nahrávání obrázku na jiný server.');
-      }
+    return res.status(500).send('Chyba při nahrávání obrázku na jiný server.');
   }
-});
+});*/
 
 module.exports = router;
