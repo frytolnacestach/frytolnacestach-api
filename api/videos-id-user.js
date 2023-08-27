3b35a90b2672fdd13b207c10cbe166cc6276eeba
frytolnacestach <frytolnacestach@gmail.com>
2023-08-27 16:45:16 +0200

    ADD videos-id-user

--- api/videos-id-user.js
+++ api/videos-id-user.js
@@ -8,13 +8,15 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 router.get('/:slug', async (req, res) => {
-  var id = req.params.slug
   try {
+    const { id } = req.query;
+    const ids = id.split(",").map((id) => parseInt(id));
 
     const { data, error } = await supabase
     .from('videos')
     .select()
-    .eq('id_user', id)
+    .in("id_user", ids)
+    .order('id', { ascending: false })
 
     res.send(JSON.stringify(data))
 
