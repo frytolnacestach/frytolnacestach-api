ebad9aab617f311df0fdaeeb68806293d0947203
frytolnacestach <frytolnacestach@gmail.com>
2023-11-01 17:07:28 +0100

    Lost password

---
+++ api/user-password-lost-new.js
@@ -0,0 +1,85 @@
+import { createClient } from '@supabase/supabase-js'
+const bcrypt = require('bcrypt')
+
+const express = require("express")
+const router = express.Router()
+
+const supabaseUrl = 'https://qdjxqerpuvcwnbiqojnv.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+router.post("/", async (req, res) => {
+    var email = req.body.email
+    var codePassword = req.body.code_password
+    var passwordNew = req.body.password_new
+
+    try {
+        const { data, error } = await supabase
+            .from('users')
+            .select()
+            .eq('email', email)
+            .eq('code_reset', codePassword)
+
+        if (error) {
+            return res.status(500).send("Server error")
+        }
+
+        if (data.length === 0) {
+            return res.status(404).send("User not found")
+        }
+
+        //Změna hesla
+        try {
+            //hash hesla
+            const hashedPassword = await bcrypt.hash(passwordNew, 10)
+
+            const { data, error } = await supabase
+                .from('users')
+                .update({
+                    password: hashedPassword,
+                })
+                .eq('email', email)
+
+            if (error) {
+                return res.status(500).send("Server error")
+            }
+
+            //znovu načtení uživatele
+            try {
+                const { data, error } = await supabase
+                    .from('users')
+                    .select()
+                    .eq('email', email)
+        
+                if (error) {
+                    return res.status(500).send("Server error")
+                }
+        
+                if (data.length === 0) {
+                    return res.status(404).send("User not found")
+                }
+        
+                const user = data[0]
+                const passwordMatchNew = await bcrypt.compare(passwordNew, user.password)
+        
+                if (!passwordMatchNew) {
+                    return res.status(401).send("Invalid password")
+                }
+
+                res.json({
+                    status: 200,
+                    message: data
+                })
+            } catch (error) {
+                return res.status(500).send("Server error")
+            }
+        } catch (error) {
+            return res.status(500).send("Server error")
+        }
+    
+    } catch (error) {
+        return res.status(500).send("Server error")
+    }
+})
+
+module.exports = router
---
+++ api/user-password-lost.js
@@ -0,0 +1,74 @@
+import { createClient } from '@supabase/supabase-js'
+
+const bcrypt = require('bcrypt')
+
+const axios = require('axios')
+
+const express = require("express")
+const router = express.Router()
+
+const supabaseUrl = 'https://qdjxqerpuvcwnbiqojnv.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+router.post("/", async (req, res) => {
+    var email = req.body.email
+
+    // Kontrola existence uživatele - email
+    const { data: existingUser, error: existingError } = await supabase
+        .from('users')
+        .select('id')
+        .eq('email', email)
+        .limit(1)
+
+    if (existingError) {
+        return res.status(500).send("Server error: " + existingError)
+    }
+
+    if (existingUser.length === 0) {
+        return res.status(400).send("Uživatel s touto e-mailovou adresou neexistuje.")
+    }
+
+    // Reset hesla
+    try {
+        // Funkce pro generování náhodného kódu
+        async function generateRandomCode(length) {
+            const saltRounds = 10
+            const code = await bcrypt.genSalt(saltRounds)
+            return code.slice(0, length)
+        }
+        const randomCode = await generateRandomCode(24)
+
+        //uložení do databaze
+        const { error } = await supabase
+            .from('users')
+            .insert({ 
+                code_reset: randomCode,
+            })
+            .eq('email', email)
+
+        if (error) {
+            return res.status(500).send("Server error")
+        }
+
+        // Odeslat e-mail o resetu hesla
+        try {
+            const response = await axios.post('https://mail.frytolnacestach.cz/api/password-lost', {
+                email: req.body.email,
+                code_password: randomCode
+            })
+
+            if (response.status === 200 || response.status === 201) {
+                return res.status(response.status).send('E-mail pro obnovu hesla byl odeslán.')
+            } else {
+                return res.status(500).send('Chyba při komunikaci s API')
+            }
+        } catch (error) {
+            return res.status(500).send('Chyba připojení k API MAIL')
+        }
+    } catch (error) {
+        return res.status(500).send("Server error")
+    }
+})
+
+module.exports = router
