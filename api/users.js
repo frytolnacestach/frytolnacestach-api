ff84b28e8e56bd36bfac5c478b0ecb0e8502cb1c
frytolnacestach <frytolnacestach@gmail.com>
2023-11-07 22:12:36 +0100

    Add filter and listing for users

--- api/users.js
+++ api/users.js
@@ -8,12 +8,55 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 router.get("/", async (req, res) => {
+    const showType = req.query.showType
+    const search = req.query.search || ''
+    const page = parseInt(req.query.page, 10)
+    const items = parseInt(req.query.items, 10)
+
+    // Setting select
+    let supabaseSelect
+    if (showType === "list"){
+        supabaseSelect = 'id, slug, nickname, surname, lastname, status'
+    } else if (showType === "search") {
+        supabaseSelect = 'id, slug, nickname'
+    } else {
+        supabaseSelect = 'id, slug, nickname, surname, lastname, status'
+    }
+
     try {
-        const { data, error } = await supabase
-            .from('users')
-            .select('id, nickname, slug, surname, lastname, status')
-            .order('id', { ascending: false })
-            .eq('status', 3)
+        let data
+        let error
+
+        if(page && items) {
+            // items filter
+            const itemsStart = (page * items) - items
+            const itemsEnd = itemsStart + items - 1
+
+            // db
+            const response = await supabase
+                .from('users')
+                .select(supabaseSelect)
+                .ilike('nickname', `%${search}%`)
+                .order('id', { ascending: false })
+                .range(itemsStart, itemsEnd)
+                .eq('status', 3)
+
+            // response
+            data = response.data
+            error = response.error
+        } else {
+            // db
+            const response = await supabase
+                .from('users')
+                .select(supabaseSelect)
+                .ilike('nickname', `%${search}%`)
+                .order('id', { ascending: false })
+                .eq('status', 3)
+
+            // response
+            data = response.data
+            error = response.error
+        }
 
         res.send(JSON.stringify(data))
     } catch (error) {
