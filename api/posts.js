4959aff70a40d82ebb3c9c65bed43efc62a3ac59
frytolnacestach <frytolnacestach@gmail.com>
2023-11-07 19:50:18 +0100

    Add filter and listing for posts

--- api/posts.js
+++ api/posts.js
@@ -8,11 +8,51 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 router.get("/", async (req, res) => {
+    const showType = req.query.showType
+    const search = req.query.search || ''
+    const page = parseInt(req.query.page, 10)
+    const items = parseInt(req.query.items, 10)
+
+    // Setting select
+    let supabaseSelect
+    if (showType === "list"){
+        supabaseSelect = 'id, id_image, slug, title, perex'
+    } else if (showType === "search") {
+        supabaseSelect = 'id, slug, title'
+    }
+
     try {
-        const { data, error } = await supabase
-            .from('posts')
-            .select()
-            .order('id', { ascending: false })
+        let data
+        let error
+
+        if(page && items) {
+            // items filter
+            const itemsStart = (page * items) - items
+            const itemsEnd = itemsStart + items - 1
+
+            // db
+            const response = await supabase
+                .from('posts')
+                .select(supabaseSelect)
+                .ilike('title', `%${search}%`)
+                .order('id', { ascending: false })
+                .range(itemsStart, itemsEnd)
+
+            // response
+            data = response.data
+            error = response.error
+        } else {
+            // db
+            const response = await supabase
+                .from('posts')
+                .select(supabaseSelect)
+                .ilike('title', `%${search}%`)
+                .order('id', { ascending: false })
+
+            // response
+            data = response.data
+            error = response.error
+        }
 
         res.send(JSON.stringify(data))
     } catch (error) {
