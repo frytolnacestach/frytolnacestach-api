4bda2330d84c403302195539672062db05898a65
frytolnacestach <frytolnacestach@gmail.com>
2023-11-10 19:23:40 +0100

    Add filter and listing for videos-id-TYPE_PLACE add type

--- api/videos-id-spot.js
+++ api/videos-id-spot.js
@@ -17,7 +17,7 @@ router.get('/:slug', async (req, res) => {
     // Setting select
     let supabaseSelect
     if (showType === "list"){
-        supabaseSelect = 'id, id_image, slug, title'
+        supabaseSelect = 'id, id_image, slug, title, type'
     } else if (showType === "search") {
         supabaseSelect = 'id, slug, title'
     }
--- api/videos.js
+++ api/videos.js
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
     // Setting select
     let supabaseSelect
     if (showType === "list"){
-        supabaseSelect = 'id, id_image, slug, title'
+        supabaseSelect = 'id, id_image, slug, title, type'
     } else if (showType === "search") {
         supabaseSelect = 'id, slug, title'
     }
