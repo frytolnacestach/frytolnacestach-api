63e3e43ac14dad6389cd11392e812931b2eafb18
frytolnacestach <frytolnacestach@gmail.com>
2024-01-15 22:05:23 +0100

    TEST - config-world-state-tabs 2

--- api/config-world-state-tabs.js
+++ api/config-world-state-tabs.js
@@ -7,18 +7,16 @@ const supabaseUrl = 'https://qdjxqerpuvcwnbiqojnv.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-router.get('/:id', async (req, res) => {
-    var id = req.params.id
+router.get('/', async (req, res) => {
+    //var id = req.params.id
     
     try {
         const { count: tabVideos, error: tabVideosError } = await supabase
             .from('videos')
-            .eq('id_state', id)
             .select('*', { count: 'exact', head: true })
 
         const { count: tabArticles, error: tabArticlesError } = await supabase
             .from('posts')
-            .eq('id_state', id)
             .select('*', { count: 'exact', head: true })
 
         const tabs = {
