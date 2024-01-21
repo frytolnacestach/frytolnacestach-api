64e3d8c670b3d0e86c4a893286d28493ff200dfa
frytolnacestach <frytolnacestach@gmail.com>
2024-01-21 12:50:35 +0100

    TEST - New JSON.parse

--- api/places-spot-create.js
+++ api/places-spot-create.js
@@ -17,18 +17,18 @@ router.post("/", async (req, res) => {
                 id_image_cover: req.body.id_image_cover,
                 id_image_hero: req.body.id_image_hero,
                 type_place: req.body.type_place,
-                type_spot: req.body.type_spot,
+                type_spot: req.body.type_spot ? JSON.parse(req.body.type_spot) : [],
                 slug: req.body.slug,
                 name: req.body.name,
                 name_original: req.body.name_original,
                 information_chatgpt: req.body.information_chatgpt,
-                information_author: req.body.information_author,
-                information_duration: req.body.information_duration,
+                information_author: req.body.information_author ? JSON.parse(req.body.information_author) : [],
+                information_duration: req.body.information_duration ? JSON.parse(req.body.information_duration) : [],
                 altitude: req.body.altitude,
-                seo_tags: req.body.seo_tags,
-                coordinates: req.body.coordinates,
-                zoom: req.body.zoom,
-                affiliate: req.body.affiliate
+                seo_tags: req.body.seo_tags ? JSON.parse(req.body.seo_tags) : [],
+                coordinates: req.body.coordinates ? JSON.parse(req.body.coordinates) : [],
+                zoom: req.body.zoom ? JSON.parse(req.body.zoom) : [],
+                affiliate: req.body.affiliate ? JSON.parse(req.body.affiliate) : [],
             })
 
         return res.status(201).send("Create Spot")
