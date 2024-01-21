20b8d7873f532f9ddf7c2f891ba5791e31d9cadf
frytolnacestach <frytolnacestach@gmail.com>
2024-01-21 13:51:30 +0100

    TEST - New JSON.parse 5

--- api/places-spot-create.js
+++ api/places-spot-create.js
@@ -22,13 +22,13 @@ router.post("/", async (req, res) => {
                 name: req.body.name,
                 name_original: req.body.name_original,
                 information_chatgpt: req.body.information_chatgpt,
-                information_author: req.body.information_author,
-                information_duration: req.body.information_duration,
+                information_author: req.body.information_author ? JSON.parse(req.body.information_author) : [],
+                information_duration: req.body.information_duration ? JSON.parse(req.body.information_duration) : [],
                 altitude: req.body.altitude,
-                seo_tags: req.body.seo_tags,
-                coordinates: req.body.coordinates,
-                zoom: req.body.zoom,
-                affiliate: req.body.affiliate
+                seo_tags: req.body.seo_tags ? JSON.parse(req.body.seo_tags) : [],
+                coordinates: req.body.coordinates ? JSON.parse(req.body.coordinates) : [],
+                zoom: req.body.zoom ? JSON.parse(req.body.zoom) : [],
+                affiliate: req.body.affiliate ? JSON.parse(req.body.affiliate) : []
             })
 
         return res.status(201).send("Create Spot")
